a97580d9d568d926e54b9798f53932018e8dd81a
Wouter Bulten <wouterbulten@mai-ai.org>
2018-06-19 11:31:31 +0200

    Optimize above the fold build

--- above-the-fold/gulpfile.js
+++ above-the-fold/gulpfile.js
@@ -4,7 +4,7 @@ var critical = require('critical').stream;
 
 // Generate & Inline Critical-path CSS
 gulp.task('critical', function () {
-    return gulp.src('../output/**/*.html')
+    return gulp.src(['../output/**/*.html', '!../output/publications/*/*.html'])
         .pipe(critical({base: '../output/', inline: true, css: ['../output/theme/css/theme.min.css']}))
         .on('error', function(err) { log.error(err.message); })
         .pipe(gulp.dest('../output'));
